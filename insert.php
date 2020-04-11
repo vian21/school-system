592d383f5c6c3953afd667d04364ab0c9d241443
Patrick Igiraneza <igiranezapatrick31@gmail.com>
2020-04-11 14:52:53 +0200

    Delete insert.php

--- insert.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'modules/config.php';
-$password=password_hash("123",PASSWORD_DEFAULT);
-$connect->query("INSERT INTO users(name,email,password,type) values('me','meteacher@gmail.com','$password',2)");
