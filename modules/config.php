86439a4a9936d82d08931a332b1328e1c2f88bda
Patrick Igiraneza <igiranezapatrick31@gmail.com>
2023-02-16 16:26:51 -0500

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -2,13 +2,13 @@
 //Configuration file
 
 //Database
-$connect = mysqli_connect('localhost', 'patrick', 'Pat31@root', 'bridge');
+$connect = mysqli_connect('localhost', 'DB_USERNAME', 'MYSQL_PASSWORD', 'DATABASE_NAME');
 
 //Configurations 
 $app_url = "http://localhost/bridge/";
 $app_name = "Bridge";
-$app_email = "igiranezapatrick31@gmail.com";
-$app_password = "pat31@solmail.com";
+$app_email = "YOUR_EMAIL";
+$app_password = "YOUR_PASSWORD";
 
 //arrays
 $genders = array('Male', 'Female');
